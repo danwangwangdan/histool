3b4437c5bf0242668333e5707cdf4590248cb778
hsm <hsm19950709@gmail.com>
2019-04-21 16:21:50 +0800

    Initial commit

---
+++ src/main/java/com/xhrmyy/histool/entity/QueueInfo.java
@@ -0,0 +1,20 @@
+package com.xhrmyy.histool.entity;
+
+import com.xhrmyy.histool.common.BaseModel;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @Description:
+ * @Author HuangShiming
+ * @Date 2019-4-21
+ */
+@Entity
+@Table(name = "v_queue_info")
+public class QueueInfo extends BaseModel {
+
+    private static final long serialVersionUID = 325141355507080613L;
+
+
+}

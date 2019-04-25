403bc9cbcc49544d1c59cdfce44d4aced654e6f2
hsm <hsm19950709@gmail.com>
2019-04-25 21:02:45 +0800

    修复bug

--- src/main/java/com/xhrmyy/histool/service/impl/QueueServiceImpl.java
+++ src/main/java/com/xhrmyy/histool/service/impl/QueueServiceImpl.java
@@ -33,43 +33,43 @@ public class QueueServiceImpl implements QueueService {
             List<QueueInfo> crudeInfo = queueUtil.getQueueInfo(office, room);
             SimpleDateFormat dd = new SimpleDateFormat("HH:mm");
             // 获取最新的叫号时间
-            Timestamp timeStamp = queueUtil.getLatestTime(office, room);
-            Date latestCallTime = new Date();
-            latestCallTime.setTime(timeStamp.getTime());
-            long diffMin = (new Date().getTime() - latestCallTime.getTime())/(1000 * 60);
-            // 设置每个叫号房间的人均等待时长，单位：分钟
-            int perWaitLength = 0;
-            if (office.equals("B超室")) {
-                if (room.equals("1号窗口")) {
-                    perWaitLength = 30;
-                } else if (room.equals("2号窗口")) {
-                    perWaitLength = 20;
-                } else if (room.equals("3号窗口")) {
-                    perWaitLength = 15;
-                } else if (room.equals("4号窗口")) {
-                    perWaitLength = 20;
-                } else {
-                    perWaitLength = 20;
-                }
-            } else if (office.equals("内窥镜室")) {
-                if (room.equals("普通胃镜")) {
-                    perWaitLength = 30;
-                } else if (room.equals("无痛胃镜")) {
-                    perWaitLength = 20;
-                } else if (room.equals("电子肠镜")) {
-                    perWaitLength = 45;
-                }else {
-                    perWaitLength = 20;
-                }
-            } else if (office.equals("心电图室")) {
-                perWaitLength = 20;
-            } else if (office.equals("脑电图室")) {
-                perWaitLength = 20;
-            } else if (office.equals("DR室")) {
-                perWaitLength = 20;
-            } else if (office.equals("CT室")) {
-                perWaitLength = 20;
-            }
+//            Timestamp timeStamp = queueUtil.getLatestTime(office, room);
+//            Date latestCallTime = new Date();
+//            latestCallTime.setTime(timeStamp.getTime());
+//            long diffMin = (new Date().getTime() - latestCallTime.getTime())/(1000 * 60);
+//            // 设置每个叫号房间的人均等待时长，单位：分钟
+//            int perWaitLength = 0;
+//            if (office.equals("B超室")) {
+//                if (room.equals("1号窗口")) {
+//                    perWaitLength = 30;
+//                } else if (room.equals("2号窗口")) {
+//                    perWaitLength = 20;
+//                } else if (room.equals("3号窗口")) {
+//                    perWaitLength = 15;
+//                } else if (room.equals("4号窗口")) {
+//                    perWaitLength = 20;
+//                } else {
+//                    perWaitLength = 20;
+//                }
+//            } else if (office.equals("内窥镜室")) {
+//                if (room.equals("普通胃镜")) {
+//                    perWaitLength = 30;
+//                } else if (room.equals("无痛胃镜")) {
+//                    perWaitLength = 20;
+//                } else if (room.equals("电子肠镜")) {
+//                    perWaitLength = 45;
+//                }else {
+//                    perWaitLength = 20;
+//                }
+//            } else if (office.equals("心电图室")) {
+//                perWaitLength = 20;
+//            } else if (office.equals("脑电图室")) {
+//                perWaitLength = 20;
+//            } else if (office.equals("DR室")) {
+//                perWaitLength = 20;
+//            } else if (office.equals("CT室")) {
+//                perWaitLength = 20;
+//            }
             //// 获取第一个人的排队序号
             //Long firstSn = crudeInfo.get(0).getSn();
             // 处理数据
@@ -79,7 +79,7 @@ public class QueueServiceImpl implements QueueService {
                 queueResult.setName(crudeInfo.get(i).getPatientName());
                 queueResult.setSn(crudeInfo.get(i).getSn());
                 queueResult.setFrontNo(i);
-                queueResult.setEsTime(String.valueOf(perWaitLength * (queueResult.getFrontNo()+1)-diffMin));
+                queueResult.setEsTime("");
                 queueResultList.add(queueResult);
             }
             baseResult.setData(queueResultList);
